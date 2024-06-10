f08505f958ed909dd65f455b9597141fee432f5c
Sneha Reddy <snehareddy9178@gmail.com>
2024-06-10 18:06:54 +0530

    add cors

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ import express from 'express';
 import { router } from './routes';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 dotenv.config();
 
 mongoose
